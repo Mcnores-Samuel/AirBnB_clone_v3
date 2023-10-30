9667a7ff2a9f58429d038de352d792aefbe63890
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-30 15:32:06 +0200

    I have initiated the api app and added neccessary route

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module for app.py and its routes """
+""" Module for app.py and its routes"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -15,11 +15,12 @@ HBNB_API_PORT = os.environ.get('HBNB_API_PORT', '5000')
 app.register_blueprint(app_views)
 
 
-
 @app.teardown_appcontext
 def teardown_appcontext(error):
-    """ Closes storage session after each request"""
+    """Closes storage session after each request"""
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=HBNB_API_HOST, port=int(HBNB_API_PORT),
+            threaded=True)
---
+++ api/v1/views/states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module for index route of the api to handle index requests
+This module handles the HTTP requests that are sent to the api
+for getting the states objects from the storage engine
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states')
+@app_views.route('/states/<state_id>')
+def states(state_id=None):
+    """ Returns a JSON response to an HTTP request"""
+    states_list = []
+    if state_id:
+        states = storage.all(State)
+        for state in states.values():
+            if state.id == state_id:
+                states_list.append(state.to_dict())
+                break
+    else:
+        states = storage.all(State)
+        for state in states.values():
+            states_list.append(state.to_dict())
+    if states_list == []:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(states_list)
